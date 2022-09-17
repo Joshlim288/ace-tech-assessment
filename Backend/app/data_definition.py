ddf0f27ba127e0b788b5add1d27cd9a49d1745ce
Josh <joshlim288@gmail.com>
2022-09-17 19:29:17 +0800

    Implement conversion of Team object to and from mongoDB document

--- Backend/app/data_definition.py
+++ Backend/app/data_definition.py
@@ -15,3 +15,27 @@ class Team:
         # used for breaking ties in group placement
         self.goalsScored = 0
         self.altPoints = 0
+    
+    '''
+    converts a Team object into a document object for mongoDB storage
+    '''
+    def toDoc(self):
+        doc = {
+            '_id': self.teamName, # convert teamName to be the id to ensure they're unique
+            'regDate': self.regDate,
+            'group': self.group,
+            'points': self.points,
+            'goalsScored': self.goalsScored,
+            'altPoints': self.altPoints
+        }
+        return doc
+    
+    '''
+    converts a team document object from mongoDB back into a Team object
+    '''
+    def fromDoc(teamDoc):
+        team = Team(teamDoc['_id'], teamDoc['regDate'], teamDoc['group'])
+        team.points = teamDoc['points']
+        team.goalsScored = teamDoc['goalsScored']
+        team.altPoints = teamDoc['altPoints']
+        return team
