ce1509a650c89ce4553386744c89565fc92cf2c4
Josh <joshlim288@gmail.com>
2022-09-18 14:11:39 +0800

    Implemented initial web application skeleton

---
+++ Frontend/lib/main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GovTech Football Competition Scoreboard',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: const MyHomePage(title: 'GovTech Football'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class Team {
+  const Team(
+    this.name,
+    this.group,
+    this.points,
+    this.goalsScored,
+    this.altPoints,
+  );
+  final String name;
+  final int group;
+  final int points;
+  final int goalsScored;
+  final int altPoints;
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Map<int, List<Team>> groups = {
+    0: [const Team('test', 0, 0, 0, 0)]
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        elevation: 1,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Text(
+            widget.title,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        actions: [
+          Row(
+            children: [
+              TextButton(onPressed: () {}, child: const Text('Enter Teams', style: TextStyle(color: Colors.black))),
+              Container(width: 10),
+              TextButton(onPressed: () {}, child: const Text('Enter Results', style: TextStyle(color: Colors.black))),
+              Container(width: 10),
+              TextButton(onPressed: () {}, child: const Text('Clear Data', style: TextStyle(color: Colors.black))),
+              Container(width: 50),
+            ],
+          )
+        ],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            children: <Widget>[
+              const Text(
+                'Scoreboard',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: '',
+                ),
+              ),
+              Container(height: 20),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'GROUP 1',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      DataTable(
+                        columns: const <DataColumn>[
+                          DataColumn(
+                            label: Text('Rank'),
+                          ),
+                          DataColumn(
+                            label: Text('Name'),
+                          ),
+                          DataColumn(
+                            label: Text('Points'),
+                          ),
+                          DataColumn(
+                            label: Text('Goals Scored'),
+                          ),
+                          DataColumn(
+                            label: Text('Alternate Points'),
+                          ),
+                        ],
+                        rows: const <DataRow>[
+                          DataRow(cells: [
+                            DataCell(Text('1')),
+                            DataCell(Text('Arshik')),
+                            DataCell(Text('5644645')),
+                            DataCell(Text('3')),
+                            DataCell(Text('265')),
+                          ])
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
