eace1fe6c00b6f071de45214c9bf221f280fb370
Josh <joshlim288@gmail.com>
2022-09-18 16:19:43 +0800

    Implement flutter delete teams API call

--- Frontend/lib/api_connection.dart
+++ Frontend/lib/api_connection.dart
@@ -7,7 +7,7 @@ class ApiConnection {
   final String baseUrl = "http://127.0.0.1:5000/";
 
   /// Enter a new batch of teams into the system
-  static Future<bool> enterTeams(String teams) async {
+  static Future<void> enterTeams(String teams, Function(String) callback) async {
     var headers = {'Content-Type': 'application/json'};
     var request = http.Request('POST', Uri.parse('http://127.0.0.1:5000/teams'));
     request.body = json.encode({"teams": teams});
@@ -15,6 +15,16 @@ class ApiConnection {
 
     http.StreamedResponse response = await request.send();
 
-    return response.statusCode == 200;
+    callback(await response.stream.bytesToString());
+  }
+
+  static Future<void> clearData(Function(String) callback) async {
+    var headers = {'Content-Type': 'application/json'};
+    var request = http.Request('DELETE', Uri.parse('http://127.0.0.1:5000/teams'));
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    callback(await response.stream.bytesToString());
   }
 }
